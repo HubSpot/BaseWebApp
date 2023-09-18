fa3446874a1dab9d015345d5baa62eb8e04996a6
Emilio Garcia <egarciav@emilios-imac.local>
2023-09-18 11:05:35 -0400

    Adding pages and navigation

--- project/start.js
+++ project/start.js
@@ -13,6 +13,14 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+
+app.get('/projects', function(request, response) {
+  response.render('pages/projects');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
